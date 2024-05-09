073744a04e5887d9bab4b4096b21c737fdd674d0
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-05-09 16:29:57 +0200

    Add project files.

---
+++ Konstruktorerövningar3inl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Konstruktorerövning3inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person jag = new Person("alex", "ved", 2001, 09, 11);
+            if(jag.isMyndig())
+            {
+                Console.WriteLine(jag.getFullnamn() + " är myndig");
+            }
+            else
+            {
+                Console.WriteLine(jag.getFullnamn() + " är inte myndig");
+            }
+        }
+    }
+}
---
+++ Konstruktorerövningar3inl/person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktorerövning3inl
+{
+    class Person
+    {
+        private string förnamn;
+        private string efternamn;
+        private string fullnamn;
+        private DateTime födelsedatum;
+
+        public Person(string F, string E, int födelseår, int födelsemånad, int födelsedag)
+        {
+            förnamn = F;
+            efternamn = E;
+            födelsedatum = new DateTime(födelseår, födelsemånad, födelsedag);
+            fullnamn = förnamn + " " + efternamn;
+        }
+        public string getFullnamn() { return fullnamn; }
+
+        public bool isMyndig()
+        {
+            DateTime eiten = DateTime.Today.AddYears(-18);
+            if (födelsedatum.CompareTo(eiten) <= 0) 
+            { 
+                return true; 
+            }
+
+            return false;
+        }
+    }
+}
